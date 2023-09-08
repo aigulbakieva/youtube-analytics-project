915d58b2a8ae9ed919905bd07fc5e55dbd18703b
aigulbakieva <bakisha.a@mail.ru>
2023-09-08 17:00:07 +0100

    Homework4. Создан класс Video Создан класс PLVideo

--- src/video.py
+++ src/video.py
@@ -3,13 +3,18 @@ from googleapiclient.discovery import build
 
 
 class Video:
-    def __init__(self, video_id):
+    def __init__(self, video_id: str):
         self.video_id: str = video_id
         self.video_info: dict = self.get_video_info(video_id)
         self.video_title: str = self.video_info['items'][0]['snippet']['title']
         self.video_url = "https://www.youtube.com/watch?v=" + f"{video_id}"
         self.view_count: int = int(self.video_info['items'][0]['statistics']['viewCount'])
         self.like_count: int = int(self.video_info['items'][0]['statistics']['likeCount'])
+
+
+    def __str__(self):
+        return self.video_title
+
 
     @classmethod
     def get_service(cls):
@@ -22,7 +27,8 @@ class Video:
                                                          id=video_id).execute()
         return video_response
 
-    class PLVideo(Video):
-        def __init__(self, video_id, playlist_id):
-            super().__init__(video_id)
-            self.playlist_id = playlist_id
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
